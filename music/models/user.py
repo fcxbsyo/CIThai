7bf30749bac31524b368824e8a4a26a5f035ff9c
Nisara Ploysuttipol <nisara.pl@ku.th>
2026-03-23 15:04:31 +0700

    make username optional and verify all API endpoints working

--- music/models/user.py
+++ music/models/user.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, blank=True, null=True, unique=True)
     google_id = models.CharField(max_length=255, null=True, blank=True)
     display_name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
